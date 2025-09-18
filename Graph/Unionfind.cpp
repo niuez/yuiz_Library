8981b418ffa8d3d856bed6fc5975128551bc54c7
yuiz-y <yui.mzn.z@gmail.com>
2025-09-18 18:38:50 +0900

    Create Unionfind.cpp

---
+++ Graph/Unionfind.cpp
@@ -0,0 +1,26 @@
+struct UnionFind{
+    private:
+    vector<int> k;
+    public:
+    int root(int i){
+        if(k[i]<0) return i;
+        else return k[i]=root(k[i]);
+    }
+    void unite(int i,int j){
+        i=root(i); j=root(j);
+        if(i==j) return;
+        if(size(i)<size(j)) swap(i,j);
+        k[i]+=k[j];
+        k[j]=i;
+        return;
+    }
+    bool connected(int x,int y){
+        return (root(x)==root(y));
+    }
+    int size(int i){
+        return k[root(i)]*-1;
+    }
+    UnionFind(int n){
+        k=vector<int>(n,-1);
+    }
+};
